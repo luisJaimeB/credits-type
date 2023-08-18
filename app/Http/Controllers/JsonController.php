c82191c77cf934b765ad6d02091895137366227e
Luis Alberto Jaime B <39748335+luisJaimeB@users.noreply.github.com>
2023-08-18 14:17:49 -0500

    Feat: New Excel import added.

---
+++ app/Http/Controllers/JsonController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Imports\CreditTypesImport;
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Http\Request;
+
+class JsonController extends Controller
+{
+    public function import() 
+    {
+        Excel::import(new CreditTypesImport, 'users.xlsx');
+        
+        return redirect('/')->with('success', 'All good!');
+    }
+}
--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Maatwebsite\Excel\ExcelServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -184,5 +184,6 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
+    'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
 ];
