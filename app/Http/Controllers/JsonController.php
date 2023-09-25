d5d31e38c44011e2abca7ed15a8ec0792d4943da
Luis Alberto Jaime B. <39748335+luisJaimeB@users.noreply.github.com>
2023-09-25 16:10:15 -0500

    Remove: The laravel excel package is removed.

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -3,15 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Imports\CreditTypesImport;
-use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Http\Request;
 
 class JsonController extends Controller
 {
-    public function import() 
-    {
-        //Excel::import(new CreditTypesImport, 'users.xlsx');
-        
-        return redirect('/')->with('success', 'All good!');
-    }
+
 }
--- config/app.php
+++ config/app.php
@@ -159,7 +159,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Maatwebsite\Excel\ExcelServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -183,7 +182,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-    ])->toArray(),
-    'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+    ])->toArray()
 
 ];
